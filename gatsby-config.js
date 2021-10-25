767daa827b95cafdc9c84e722562e554d77c69ea
matiasracedo <matiasracedo@gmail.com>
2021-10-25 12:00:45 -0300

    changed analytics version

--- gatsby-config.js
+++ gatsby-config.js
@@ -149,15 +149,36 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`,
     },
     "gatsby-plugin-robots-txt",
+    //  {
+    //    resolve: "gatsby-plugin-google-analytics",
+    //    options: {
+    //      trackingId: process.env.GOOGLE_TRACKING_ID, // Google Analytics Tracking ID
+    //      head: true,
+    //      respectDNT: true,
+    //      cookieDomain: "hotelmainumby.com", // Your Domain
+    //    },
+    //  },
      {
-       resolve: `gatsby-plugin-google-analytics`,
-       options: {
-         trackingId: process.env.GOOGLE_TRACKING_ID, // Google Analytics Tracking ID
-         head: true,
-         respectDNT: true,
-         cookieDomain: "hotelmainumby.com/", // Your Domain
-       },
-     },
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_TRACKING_ID // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-web-font-loader",
